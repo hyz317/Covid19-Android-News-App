57336488efeba40c577c34b55892e415d07a0a78
hyz317 <luoyumiling@126.com>
2020-09-06 17:31:19 +0800

    增加增删分类键

--- MorphlingNews/app/src/main/java/com/java/heyuze/ui/news/NewsFragment.java
+++ MorphlingNews/app/src/main/java/com/java/heyuze/ui/news/NewsFragment.java
@@ -1,10 +1,14 @@
 package com.java.heyuze.ui.news;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +16,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.java.heyuze.Activity.MainActivity;
 import com.java.heyuze.R;
+import com.google.android.material.tabs.TabLayout;
+
+import org.w3c.dom.Text;
 
 public class NewsFragment extends Fragment {
 
@@ -30,6 +38,23 @@ public class NewsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Button adjustCategoryBtn = root.findViewById(R.id.category);
+        adjustCategoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog2 = new AlertDialog.Builder(getActivity())
+                        .setMessage("TODO")
+                        .setNegativeButton("完成", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                // TODO Auto-generated method stub
+                            }
+                        }).show();
+                alertDialog2.show();
+            }
+        });
+
         return root;
     }
 }
