3d843629fc1dfe2fe6274518320c1403f264f2a2
hyz317 <luoyumiling@126.com>
2020-09-07 19:22:55 +0800

    真的新闻列表+详情页

--- MorphlingNews/app/src/main/java/com/java/heyuze/Activity/NewsActivity.java
+++ MorphlingNews/app/src/main/java/com/java/heyuze/Activity/NewsActivity.java
@@ -1,4 +1,35 @@
 package com.java.heyuze.Activity;
 
-public class NewsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.java.heyuze.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newscontent);
+        initView();
+    }
+
+    public void initView(){
+        TextView tvTitle = (TextView) this.findViewById(R.id.tv_title);
+        TextView tvDate = (TextView) this.findViewById(R.id.tv_date);
+        TextView tvContent = (TextView) this.findViewById(R.id.tv_content);
+
+        Intent intent = this.getIntent();
+        String title = intent.getStringExtra("title");
+        String date = intent.getStringExtra("date");
+        String content = intent.getStringExtra("content");
+
+        tvTitle.setText(title);
+        tvDate.setText(date);
+        tvContent.setText(content);
+    }
 }
+
--- MorphlingNews/app/src/main/java/com/java/heyuze/ui/news/NewsFragment.java
+++ MorphlingNews/app/src/main/java/com/java/heyuze/ui/news/NewsFragment.java
@@ -3,13 +3,17 @@ package com.java.heyuze.ui.news;
 import android.animation.LayoutTransition;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,13 +25,20 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.java.heyuze.Activity.MainActivity;
+import com.java.heyuze.Activity.NewsActivity;
+import com.java.heyuze.Adapter.NewsAdapter;
+import com.java.heyuze.InfoManager;
+import com.java.heyuze.NewsContent;
+import com.java.heyuze.NewsData;
 import com.java.heyuze.R;
 import com.google.android.material.tabs.TabLayout;
+import com.java.heyuze.knowledgeData;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class NewsFragment extends Fragment {
 
@@ -92,6 +103,8 @@ public class NewsFragment extends Fragment {
         newsViewModel =
                 ViewModelProviders.of(this).get(NewsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_news, container, false);
+
+        /*
         final TextView textView = root.findViewById(R.id.text_home);
         newsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -99,7 +112,7 @@ public class NewsFragment extends Fragment {
                 textView.setText(s);
             }
         });
-
+        */
 
         lookingCategories = new ArrayList<String>() {{ add("类别1"); add("类别2"); add("类别3");
                                                      add("类别4"); add("类别5"); add("类别6"); }};
@@ -154,6 +167,57 @@ public class NewsFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 alertDialog2.show();
+            }
+        });
+
+        try
+        {
+            while (!InfoManager.getInstance().hasNewsData())
+            {
+                System.out.println("Please Wait!");
+            }
+            NewsContent test = InfoManager.getInstance().getNewsContent("5e8d92fa7ac1f2cf57f7bc75");
+            System.out.println(test.content);
+            System.out.println(test.date);
+            System.out.println(test.labels.get(0));
+            System.out.println(test.words.get(0));
+            Vector<knowledgeData> test2 = InfoManager.getInstance().getKnowledge("病毒");
+            System.out.println(test2.get(0).description);
+            System.out.println(test2.get(0).url);
+            System.out.println(test2.get(0).hot);
+            System.out.println(test2.get(0).imgUrl);
+            System.out.println(test2.get(0).label);
+            System.out.println(test2.get(0).properties.get("鉴别诊断"));
+            System.out.println(test2.get(0).forwardRelations.get("无症状感染者"));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        final Vector<NewsData> data = InfoManager.getInstance().getNewsData();
+        NewsAdapter adapter = new NewsAdapter(getActivity(), data);
+        ListView listView = root.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+                Intent intent = new Intent(getActivity(), NewsActivity.class);
+
+                try {
+                    NewsContent ctn = InfoManager.getInstance().getNewsContent(data.get(position).id);
+                    intent.putExtra("title", data.get(position).title);
+                    intent.putExtra("date", data.get(position).time);
+                    intent.putExtra("content", ctn.content);
+                } catch (Exception e) {
+                    System.out.println("EXCEPTION GET NEWS CONTENT?");
+                }
+
+                getActivity().startActivity(intent);
+
             }
         });
 
